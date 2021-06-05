edef039dd109f9f38a07e44f4ed9982f8c67e04e
Naoto Takagi <naoto.yu@gmail.com>
2021-06-05 21:53:33 +0900

    表示を調整

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
 	def index
+		@imposter_top_users = User.limit(10).order(imposter_rate: "DESC")
+		@crew_top_users = User.limit(10).order(crew_rate: "DESC")
 	end
 	def show
 		@matches = Match.all.order(id: "DESC")
